79bf415b3a80d4e8523be1d1961df4c915598aec
LIOTINE Camille <cel2717@quser22.quest.it.northwestern.edu>
2019-07-25 15:43:56 -0500

    corrected output of hfrac column

--- cosmic/src/bpp_array.f
+++ cosmic/src/bpp_array.f
@@ -3,7 +3,7 @@
      &                      mass1,mass2,kstar1,kstar2,sep,
      &                      tb,ecc,rrl1,rrl2,bkick,
      &                      aj1,aj2,tms1,tms2,
-     &                      massc1,massc2,rad1,rad2,zpars)
+     &                      massc1,massc2,rad1,rad2,hfrac)
         IMPLICIT NONE
         INCLUDE 'const_bse.h'
 *
@@ -12,7 +12,7 @@
 *     Author : Scott Coughlin
 *     Date :   12th March 2019
 *
-        REAL*8 bkick(20),mass1,mass2,zpars(20)
+        REAL*8 bkick(20),mass1,mass2,hfrac
         REAL*8 evolve_type,sep,tb,ecc,tphys,rrl1,rrl2
         REAL*8 aj1,aj2,tms1,tms2,massc1,massc2,rad1,rad2
         INTEGER jp,jj
@@ -50,7 +50,7 @@
         bpp(jp,21) = massc2
         bpp(jp,22) = rad1
         bpp(jp,23) = rad2
-        bpp(jp,24) = zpars(11)
+        bpp(jp,24) = hfrac
         END
 
 ***
@@ -62,7 +62,7 @@
      &                      menv_2,renv_2,epoch_2,ospin_2,deltam_2,
      &                      RROL_2,porb,sep,ecc,B_0_1,B_0_2,SNkick_1,
      &                      SNkick_2, Vsys_final,SNtheta_final,
-     &                      SN_1,SN_2,bin_state,merger_type,zpars)
+     &                      SN_1,SN_2,bin_state,merger_type,hfrac)
         IMPLICIT NONE
         INCLUDE 'const_bse.h'
 *
@@ -71,7 +71,7 @@
 *     Author : Scott Coughlin
 *     Date :   12th March 2019
 *
-        REAL*8 tphys,mass0_1,mass_1,lumin_1,rad_1,teff_1,zpars(20)
+        REAL*8 tphys,mass0_1,mass_1,lumin_1,rad_1,teff_1,hfrac
         REAL*8 massc_1,radc_1,menv_1,renv_1,epoch_1
         REAL*8 ospin_1,deltam_1,RROL_1
         REAL*8 mass0_2,mass_2,lumin_2,rad_2,teff_2,massc_2
@@ -126,6 +126,6 @@
         bcm(ip,40) = float(SN_2)
         bcm(ip,41) = bin_state
         bcm(ip,42) = merger_type
-        bcm(ip,43) = zpars(11)
+        bcm(ip,43) = hfrac
 
         END
