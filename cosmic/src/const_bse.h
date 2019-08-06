71b242df20912be2873b6b1fa87e53433a710381
LIOTINE Camille <cel2717@quser21.quest.it.northwestern.edu>
2019-08-06 16:18:38 -0500

    forgot to commit change to const bse

--- cosmic/src/const_bse.h
+++ cosmic/src/const_bse.h
@@ -35,6 +35,6 @@
       COMMON /TSTEPC/ dmmax,drmax
       REAL*8 scm(50000,14),spp(20,3)
       COMMON /SINGLE/ scm,spp
-      REAL*8 bcm(50000,42),bpp(1000,23)
+      REAL*8 bcm(50000,43),bpp(1000,24)
       COMMON /BINARY/ bcm,bpp
 *
